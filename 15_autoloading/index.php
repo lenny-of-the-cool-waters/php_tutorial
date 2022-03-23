ab30cc72733dd3f3dc3327c62f0d39cde981d9a3
lenny-of-the-cool-waters <lennygith@gmail.com>
2022-03-23 08:24:50 +0300

    Fixed package autoimport

--- 15_autoloading/index.php
+++ 15_autoloading/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// require_once 'vendor/autoload.php';
+require_once 'vendor/autoload.php';
 /* require_once 'app/Email.php';
 require_once 'app/Person.php'; */
 
@@ -8,10 +8,11 @@ require_once 'app/Person.php'; */
 $person = new app\Person;
  */
 
-$client = new \GuzzleHttp\Client();
+$client = new GuzzleHttp\Client();
 $response = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
 
 echo $response->getStatusCode(); // 200
 echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
 echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
+
 ?>
